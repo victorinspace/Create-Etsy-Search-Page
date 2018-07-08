506d02edd72ddb140936f952de26d7dc7d7353a4
Victor Velasquez <victorv24@gmail.com>
2018-07-08 12:10:39 -0700

    added images and created box-shadows

--- main.js
+++ main.js
@@ -1,5 +1,21 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// creating a variable to interact with the page
 const main = document.querySelector('#main-section__right')
 
+// DOM elements to be added
 const etsyItems = items.results.map(function(item) {
 	return `
 		<a class="product" href="${unescape(item.url)}">
@@ -10,7 +26,13 @@ const etsyItems = items.results.map(function(item) {
 	`
 }).join('\n')
 
+// adding new elements to the page
 main.innerHTML = etsyItems
+
+
+
+
+
 
 // placeholder image
 // http://placehold.it/340x270
