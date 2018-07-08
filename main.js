67bbe280f895ef7adc06685403cdcb9f79623c56
Victor Velasquez <victorv24@gmail.com>
2018-07-08 10:56:37 -0700

    dynamic data has been added

--- main.js
+++ main.js
@@ -1,16 +1,23 @@
-// console.log(items[0])
+// const productImage = items.results[0].Images
+// console.log(productImage)
 
+// title
+// const productName = items.results.forEach(item => )
+// console.log(productName)
 
+// const productPrice = items.results[0].price
+// console.log(productPrice)
 
-// const main = document.querySelector("#content")
+const main = document.querySelector('#main-section__right')
 
-// const etsyItems = items.map(function(item) {
-// 	return 
-// 	`
-// 	<a class="product" href="${unescapeitem.url}">
-// 		<img src="">
-// 		<span class="title">${item.title}</span>
-// 		<span class="price">${item.price.toFixed(2)}</span>
-// 	</a>
-// 	`
-// })
+const etsyItems = items.results.map(function(item) {
+	return `
+		<a class="product" href="${unescape(item.url)}">
+			<img src="http://placehold.it/340x270">
+			<span class="title">${item.title}</span>
+			<span class="price">${item.price}</span>
+		</a>
+	`
+}).join('\n')
+
+main.innerHTML = etsyItems
