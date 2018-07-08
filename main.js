286f7e6535ad35a4b7506c73f7d82057f29af4d2
Victor Velasquez <victorv24@gmail.com>
2018-07-08 11:52:09 -0700

    added images data

--- main.js
+++ main.js
@@ -1,19 +1,9 @@
-// const productImage = items.results[0].Images
-// console.log(productImage)
-
-// title
-// const productName = items.results.forEach(item => )
-// console.log(productName)
-
-// const productPrice = items.results[0].price
-// console.log(productPrice)
-
 const main = document.querySelector('#main-section__right')
 
 const etsyItems = items.results.map(function(item) {
 	return `
 		<a class="product" href="${unescape(item.url)}">
-			<img src="http://placehold.it/340x270">
+			<img src="${item.Images[0].url_fullxfull}">
 			<span class="title">${item.title}</span>
 			<span class="price">${item.price}</span>
 		</a>
@@ -21,3 +11,6 @@ const etsyItems = items.results.map(function(item) {
 }).join('\n')
 
 main.innerHTML = etsyItems
+
+// placeholder image
+// http://placehold.it/340x270
